15127125ed18d18e8fe2362e3cca7050eb7627c6
Bilal El Moudden <belel22@jaumebalmes.net>
2025-06-02 15:25:24 +0200

    exercici1

--- conexio/conexio.py
+++ conexio/conexio.py
@@ -1,1 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+db_user = 'postgres'
+db_port = 5432
+db_host = 'localhost'
+db_password = 'admin'
+db_name = 'examenRecuFastApi'
+
+uri = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+
+engine = create_engine(uri)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base
